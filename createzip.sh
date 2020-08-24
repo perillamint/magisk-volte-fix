40560e159ccf0b3cef44205b0860f752fce7b8c2
perillamint <yhban@kdml.co.kr>
2020-08-24 18:23:04 +0900

    Initial commit

---
+++ createzip.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf system system32 system64
+
+wget https://github.com/KhushrajRathod/VoLTE-Fix/releases/download/v5/GSI-Qualcomm-VoLTE-Fix.zip -O volte-fix.zip
+unzip volte-fix.zip 'system*/*' -d .
+rm -f volte-fix.zip
+
+zip -x 'createzip.sh' -x '*.zip' -r9 volte.zip *
---
+++ customize.sh
@@ -0,0 +1,16 @@
+SKIPUNZIP=1
+
+unzip -o "$ZIPFILE" -x 'META-INF/*' -x 'system32/*' -x 'system64/*' -d "$MODPATH" >&2
+
+ARCHFILE="system32"
+
+if [ "$IS64BIT" == "true" ]; then
+    ARCHFILE="system64"
+fi
+
+ui_print "Installing $ARCHFILE"
+
+unzip -o "$ZIPFILE" "$ARCHFILE/*" -d "$TMPDIR"
+cp -r "$TMPDIR/$ARCHFILE/*" "$MODPATH/system/"
+
+ui_print "Installation successful!"
