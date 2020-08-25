2b2a0601b1fbd65654efcc3402823a6230462a57
perillamint <yhban@kdml.co.kr>
2020-08-25 20:21:01 +0900

    SELinux, fuck you.

--- customize.sh
+++ customize.sh
@@ -1,6 +1,6 @@
 SKIPUNZIP=1
 
-unzip -o "$ZIPFILE" -x 'META-INF/*' -x 'system32/*' -x 'system64/*' -d "$MODPATH" >&2
+unzip -o "$ZIPFILE" -x 'META-INF/*' -x 'system32/*' -x 'system64/*' -x 'system-lmi/*' -d "$MODPATH" >&2
 
 ARCHFILE="system32"
 
@@ -12,5 +12,7 @@ ui_print "Installing $ARCHFILE"
 
 unzip -o "$ZIPFILE" "$ARCHFILE/*" -d "$TMPDIR"
 cp -r "$TMPDIR/$ARCHFILE/"* "$MODPATH/system/"
+
+set_perm_recursive $MODPATH 0 0 0755 0644 u:object_r:system_file:s0
 
 ui_print "Installation successful!"
